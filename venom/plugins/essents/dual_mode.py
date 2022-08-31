7a0418f6de74b5df23d25917c96f086436acfeb7
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-08-31 15:55:16 +0530

    Update dual_mode.py

--- venom/plugins/essents/dual_mode.py
+++ venom/plugins/essents/dual_mode.py
@@ -52,4 +52,4 @@ async def dual_mode(_, message: MyMessage):
         {'_id': 'USER_MODE'}, {'$set': {'switch': Config.USER_MODE}}, upsert=True
     )
     await message.edit(f"Mode changed to: <b>{mode_}</b>")
-    MyDecorator.my_decorator()
+#    MyDecorator.my_decorator()
