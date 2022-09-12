0a42032ad925943cf81c88546208907659fbb014
Kakashi <ashwinstr@gmail.com>
2022-09-12 21:43:51 +0530

    testing datetime

--- venom/plugins/helpful/datetime.py
+++ venom/plugins/helpful/datetime.py
@@ -4,19 +4,42 @@ from pytz import timezone as tz, UnknownTimeZoneError, country_timezones as ct, 
 from datetime import datetime
 
 from venom import venom, MyMessage, Config
-from venom.helpers import paste_it
+from venom.helpers import paste_it, plugin_name
 
+HELP_ = Config.HELP[plugin_name(__name__)] = {'type': 'helpful', 'commands': []}
 COUNTRY_LIST = ""
 
 async def _init() -> None:
     list_ = ""
     for one in cn.keys():
+        total = 0
         list_ += f"{one}: {cn.get(one)}\n"
+        try:
+            for two in ct[one]:
+                total += 1
+                list_ += f"{total} {two}\n"
+        except KeyError:
+            pass
+        list_ += "\n"
     list_ = "LIST OF TIMEZONE:\n\n" + list_
     link_ = await paste_it(list_)
     global COUNTRY_LIST
     COUNTRY_LIST = link_
 
+#########################################################################################################################################################
+
+HELP_['commands'].append(
+    {
+        'command': 'dt',
+        'flags': None,
+        'usage': 'Check time and date',
+        'syntax': (
+            '{tr}dt [country code] [timezone number]\n'
+            'see help with "{tr}dt help"'
+        ),
+        'sudo': True
+    }
+)
 
 @venom.trigger(r'dt ?(\w*)?\s?(\d*)?')
 async def date_time(_, message: MyMessage):
@@ -26,7 +49,6 @@ async def date_time(_, message: MyMessage):
         country = match_.group(1) or "in"
         country = country.upper()
         if country == "HELP":
-            await message.reply(cn.keys())
             return await message.edit(f"Country list: **[LINK]({COUNTRY_LIST})**")
         num = match_.group(2) or 1
     try:
@@ -34,7 +56,7 @@ async def date_time(_, message: MyMessage):
     except UnknownTimeZoneError:
         return await message.edit(f"`Unknown timezone.`\nSee country list **[HERE]({COUNTRY_LIST})**", del_in=5)
     except IndexError:
-        text_ = f"#<b>INDEX_ERROR\n\n</b>Country <b>{country}</b>'s timezones:" + " [<b>{}</b>]\n"
+        text_ = f"#<b>INDEX_ERROR</b>\n\n</b>Country <b>{country}</b>'s timezones:" + " [<b>{}</b>]\n"
         total = 0
         for one in ct[country]:
             total += 1
@@ -42,4 +64,4 @@ async def date_time(_, message: MyMessage):
         return await message.edit(text_.format(total))
     format_ = "%d-%b, %Y </b>at<b> %I:%M:%S %p</b> on <b>%A"
     dt_ = datetime.now().astimezone(tz_).strftime(format_)
-    await message.edit(f"Timezone <b>({ct[country][int(num) - 1]})</b>: <b>{dt_}</b>")
+    await message.edit(f"Timezone <b>{cn.get(country)}</b>({ct[country][int(num) - 1]}): <b>{dt_}</b>")
