57b63cb3269c3a2564da15e67f03042a3a21b354
Kakashi <ashwinstr@gmail.com>
2023-04-02 20:53:55 +0530

    reverting to 1.5.0 pyromod

--- venom/core/methods/message/listen.py
+++ venom/core/methods/message/listen.py
@@ -1,6 +1,9 @@
 # listen.py
 
-from pyrogram import Client as RClient, filters
+from typing import Union
+
+from pyrogram import Client as RClient
+from pyrogram import filters
 
 from ... import types
 
@@ -8,21 +11,24 @@ from ... import types
 class Listen(RClient):
 
     async def listen(self,
+                     chat_id: Union[str, int],
                      timeout: int = 15,
                      filters_: filters.Filter = None) -> 'types.message.MyMessage':
         """ custom listener for VenomX """
 
-        msg = await super().listen(timeout=timeout, filters=filters_)
+        msg = await super().listen(chat_id=chat_id, timeout=timeout, filters=filters_)
 
         return types.message.MyMessage.parse(msg)
 
     async def ask(self,
                   text: str,
+                  chat_id: Union[str, int],
                   timeout: int = 15,
                   filters_: filters.Filter = None) -> 'types.message.MyMessage':
         """ custom ask for VenomX """
 
-        msg = await super().ask(text=text,
+        msg = await super().ask(chat_id=chat_id,
+                                text=text,
                                 timeout=timeout,
                                 filters=filters_)
 
